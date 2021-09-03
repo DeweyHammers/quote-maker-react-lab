4e9b18f155b96a769ba0876a598c71943db6b1c0
dewey <hammersdrichard@gmail.com>
2021-09-03 07:06:35 -0700

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,56 +1,69 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import uuid from 'uuid';
-
-import { addQuote } from '../actions/quotes';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import uuid from "uuid";
+import { addQuote } from "../actions/quotes";
 
 class QuoteForm extends Component {
-
   state = {
-    //set up a controlled form with internal state
-  }
+    content: "",
+    author: "",
+  };
 
-  handleOnChange = event => {
-    // Handle Updating Component State
-  }
+  handleOnChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-  handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
-  }
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addQuote({
+      id: uuid(),
+      content: this.state.content,
+      author: this.state.author,
+      votes: 0,
+    });
+    this.setState({ content: "", author: "" });
+  };
 
   render() {
     return (
-      <div className="container">
+      <div className="container" onSubmit={this.handleOnSubmit}>
         <div className="row">
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
                 <form className="form-horizontal">
                   <div className="form-group">
-                    <label htmlFor="content" className="col-md-4 control-label">Quote</label>
+                    <label htmlFor="content" className="col-md-4 control-label">
+                      Quote
+                    </label>
                     <div className="col-md-5">
                       <textarea
+                        name="content"
                         className="form-control"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
                   <div className="form-group">
-                    <label htmlFor="author" className="col-md-4 control-label">Author</label>
+                    <label htmlFor="author" className="col-md-4 control-label">
+                      Author
+                    </label>
                     <div className="col-md-5">
                       <input
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button type="submit" className="btn btn-default">
+                        Add
+                      </button>
                     </div>
                   </div>
                 </form>
@@ -63,5 +76,4 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, { addQuote })(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
-}
+  switch (action.type) {
+    case "ADD_QUOTE":
+      const newQuote = {
+        id: action.quote.id,
+        content: action.quote.content,
+        author: action.quote.author,
+        votes: action.quote.votes,
+      };
+      return (state = [...state, newQuote]);
+    case "REMOVE_QUOTE":
+      return (state = state.filter((quote) => quote.id !== action.quoteId));
+    case "UPVOTE_QUOTE":
+      return (state = state.map((quote) => {
+        quote.id === action.quoteId && (quote.votes += 1);
+        return quote;
+      }));
+    case "DOWNVOTE_QUOTE":
+      return (state = state.map((quote) => {
+        quote.id === action.quoteId && quote.votes !== 0 && (quote.votes -= 1);
+        return quote;
+      }));
+    default:
+      return state;
+  }
+};
